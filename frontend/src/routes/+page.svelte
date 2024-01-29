9b86dfa098d8cd33c73609eb6a00416d7d3b47a7
den138 <dennisming2012@gmail.com>
2024-01-29 11:33:37 +0800

    chore: minor update

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -4,7 +4,7 @@
 	import Cookies from 'js-cookie';
 	import { writable, type Writable } from 'svelte/store';
 
-	export const spotifyData: Writable<any | null> = writable(null);
+	export const data: Writable<any | null> = writable(null);
 	export const isLoading = writable(true);
 
 	onMount(() => {
@@ -20,7 +20,7 @@
 				if (tokenObj.accessToken) {
 					try {
 						const data = await getSpotifyMe();
-						spotifyData.set(data);
+						data.set(data);
 					} catch (error) {
 						console.error('Failed to fetch data from Spotify', error);
 					}
@@ -33,25 +33,24 @@
 		init();
 	});
 
-	async function oauth() {
+	async function oauth(): Promise<void> {
 		const response = await fetch('http://localhost:8080/auth');
 		const url = await response.text();
 		window.location.href = url;
 	}
 
-	async function getSpotifyMe() {
-		const response = await fetch('https://api.spotify.com/v1/me', {
+	async function getSpotifyMe(): Promise<any> {
+		const resp = await fetch('https://api.spotify.com/v1/me', {
 			headers: {
 				Authorization: `Bearer ${$token?.accessToken}`
 			}
 		});
 
-		if (!response.ok) {
-			throw new Error(`HTTP error! status: ${response.status}`);
+		if (!resp.ok) {
+			throw new Error(`HTTP error! status: ${resp.status}`);
 		}
 
-		const data = await response.json();
-		return data;
+		return await resp.json();
 	}
 
 	$: isLoggedIn = $token?.accessToken !== undefined;
@@ -61,10 +60,10 @@
 	<p>Loading...</p>
 {:else if !isLoggedIn}
 	<button on:click={oauth}>Login Spotified</button>
-{:else if $spotifyData}
-	<h1>Welcome, {$spotifyData.display_name}</h1>
-	<div><a href={$spotifyData.external_urls.spotify}>{$spotifyData.external_urls.spotify}</a></div>
-	<img src={$spotifyData.images[1].url} alt="User Profile" />
-	<h3>Followers: {$spotifyData.followers.total}</h3>
-	<h2>{JSON.stringify($spotifyData)}</h2>
+{:else if $data}
+	<h1>Welcome, {$data.display_name}</h1>
+	<div><a href={$data.external_urls.spotify}>{$data.external_urls.spotify}</a></div>
+	<img src={$data.images[1].url} alt="User Profile" />
+	<h3>Followers: {$data.followers.total}</h3>
+	<h2>{JSON.stringify($data)}</h2>
 {/if}
