b5f141798be67adea67262f47f711e5a6784e3d3
Dennis Chan <dennisming2012@gmail.com>
2024-03-06 12:12:10 +0800

    Add memoize function

--- auth.util.ts
+++ auth.util.ts
@@ -1,3 +1,5 @@
+import pMemoize from 'p-memoize';
+
 interface SpotifyTokenResponse {
   access_token: string;
   token_type: string;
@@ -6,7 +8,8 @@ interface SpotifyTokenResponse {
   scope: string;
 }
 
-export default async function refreshAccessToken(refreshToken: string) {
+const refreshAccessToken = pMemoize(async (refreshToken: string) => {
+  console.log(`${Date.now()}: refreshing token with ${refreshToken}`);
   try {
     const url = process.env.SPOTIFY_API_TOKEN_URL!;
     const response = await fetch(url, {
@@ -29,7 +32,7 @@ export default async function refreshAccessToken(refreshToken: string) {
       await response.json();
 
     console.log(
-      `fetched refreshedToken, expires in ${getTokenExpiresAt(expires_in)}`
+      `fetched refreshedToken expires in ${getTokenExpiresAt(expires_in)}`
     );
     return {
       accessToken: access_token,
@@ -42,7 +45,45 @@ export default async function refreshAccessToken(refreshToken: string) {
       error: 'RefreshAccessTokenError' as const,
     };
   }
-}
+});
+
+// export default async function refreshAccessToken(refreshToken: string) {
+//   try {
+//     const url = process.env.SPOTIFY_API_TOKEN_URL!;
+//     const response = await fetch(url, {
+//       method: 'POST',
+//       headers: {
+//         'Content-Type': 'application/x-www-form-urlencoded',
+//         Authorization: `Basic ${Buffer.from(
+//           process.env.SPOTIFY_CLIENT_ID +
+//             ':' +
+//             process.env.SPOTIFY_CLIENT_SECRET
+//         ).toString('base64')}`,
+//       },
+//       body: new URLSearchParams({
+//         grant_type: 'refresh_token',
+//         refresh_token: refreshToken,
+//       }),
+//     });
+
+//     const { access_token, expires_in, refresh_token }: SpotifyTokenResponse =
+//       await response.json();
+
+//     console.log(
+//       `fetched refreshedToken, expires in ${getTokenExpiresAt(expires_in)}`
+//     );
+//     return {
+//       accessToken: access_token,
+//       expiresAt: getTokenExpiresAt(expires_in),
+//       refreshToken: refresh_token,
+//     };
+//   } catch (e) {
+//     console.log(e);
+//     return {
+//       error: 'RefreshAccessTokenError' as const,
+//     };
+//   }
+// }
 
 function getTokenExpiresAt(expiresIn: number): number {
   return getCurrentUnixTimestampInSeconds() + expiresIn;
@@ -51,3 +92,5 @@ function getTokenExpiresAt(expiresIn: number): number {
 function getCurrentUnixTimestampInSeconds(): number {
   return Math.floor(Date.now() / 1000);
 }
+
+export default refreshAccessToken;
