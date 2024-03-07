a69f8494fafcaa8f729df6051567592bc71a421f
Dennis Chan <dennisming2012@gmail.com>
2024-03-07 17:25:19 +0800

    Remove some libraries

--- auth.util.ts
+++ auth.util.ts
@@ -1,5 +1,3 @@
-import pMemoize from 'p-memoize';
-
 interface SpotifyTokenResponse {
   access_token: string;
   token_type: string;
@@ -8,8 +6,7 @@ interface SpotifyTokenResponse {
   scope: string;
 }
 
-const refreshAccessToken = pMemoize(async (refreshToken: string) => {
-  console.log(`${Date.now()}: refreshing token with ${refreshToken}`);
+export default async function refreshAccessToken(refreshToken: string) {
   try {
     const url = process.env.SPOTIFY_API_TOKEN_URL!;
     const response = await fetch(url, {
@@ -32,7 +29,7 @@ const refreshAccessToken = pMemoize(async (refreshToken: string) => {
       await response.json();
 
     console.log(
-      `fetched refreshedToken expires in ${getTokenExpiresAt(expires_in)}`
+      `fetched refreshedToken, expires in ${getTokenExpiresAt(expires_in)}`
     );
     return {
       accessToken: access_token,
@@ -45,45 +42,7 @@ const refreshAccessToken = pMemoize(async (refreshToken: string) => {
       error: 'RefreshAccessTokenError' as const,
     };
   }
-});
-
-// export default async function refreshAccessToken(refreshToken: string) {
-//   try {
-//     const url = process.env.SPOTIFY_API_TOKEN_URL!;
-//     const response = await fetch(url, {
-//       method: 'POST',
-//       headers: {
-//         'Content-Type': 'application/x-www-form-urlencoded',
-//         Authorization: `Basic ${Buffer.from(
-//           process.env.SPOTIFY_CLIENT_ID +
-//             ':' +
-//             process.env.SPOTIFY_CLIENT_SECRET
-//         ).toString('base64')}`,
-//       },
-//       body: new URLSearchParams({
-//         grant_type: 'refresh_token',
-//         refresh_token: refreshToken,
-//       }),
-//     });
-
-//     const { access_token, expires_in, refresh_token }: SpotifyTokenResponse =
-//       await response.json();
-
-//     console.log(
-//       `fetched refreshedToken, expires in ${getTokenExpiresAt(expires_in)}`
-//     );
-//     return {
-//       accessToken: access_token,
-//       expiresAt: getTokenExpiresAt(expires_in),
-//       refreshToken: refresh_token,
-//     };
-//   } catch (e) {
-//     console.log(e);
-//     return {
-//       error: 'RefreshAccessTokenError' as const,
-//     };
-//   }
-// }
+}
 
 function getTokenExpiresAt(expiresIn: number): number {
   return getCurrentUnixTimestampInSeconds() + expiresIn;
@@ -92,5 +51,3 @@ function getTokenExpiresAt(expiresIn: number): number {
 function getCurrentUnixTimestampInSeconds(): number {
   return Math.floor(Date.now() / 1000);
 }
-
-export default refreshAccessToken;
--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   plugins: {
-    tailwindcss: {},
+    // reference: https://stackoverflow.com/questions/77788615/next-js-uncaught-syntaxerror-invalid-or-unexpected-token-then-chunkloaderror
+    // tailwindcss: {},
     autoprefixer: {},
   },
 };
